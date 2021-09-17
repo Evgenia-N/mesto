dd3bcd108f70f6042d9c962e065e7691193c4748
Evgenia-N <jazzpenguin@yandex.ru>
2021-09-18 02:34:32 +0300

    feat: всплывающее окно

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let popup = document.querySelector(".popup");
+let popupOpen = document.querySelector(".profile__edit-button");
+let popupClose = popup.querySelector(".popup__close-button");
+let formElement = document.querySelector(".popup__container");
+let profName = document.querySelector(".profile__title");
+let profJob = document.querySelector(".profile__subtitle");
+let nameInput = formElement.querySelector(".popup__field-name");
+let jobInput = formElement.querySelector(".popup__field-about-self");
+
+function popupToggle() {
+  if (popup.classList.contains("popup_opened") === false) {
+    popup.classList.add("popup_opened");
+    nameInput.value = profName.textContent;
+    jobInput.value = profJob.textContent;
+  } else {
+    popup.classList.remove("popup_opened");
+  }
+}
+
+function clickOverlay(event) {
+  if (event.target === event.currentTarget) {
+    popupToggle();
+  }
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profName.textContent = nameInput.value;
+  profJob.textContent = jobInput.value;
+  popupToggle();
+}
+
+popup.addEventListener("click", clickOverlay);
+popupOpen.addEventListener("click", popupToggle);
+popupClose.addEventListener("click", popupToggle);
+formElement.addEventListener("submit", formSubmitHandler);
