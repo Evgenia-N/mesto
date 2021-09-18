8d043f883247591cb499e5f6addba2df6fb3b530
Evgenia-N <jazzpenguin@yandex.ru>
2021-09-18 22:08:44 +0300

    feat: реализован поп-ап с формой и кнопкой отправки данных

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,33 @@
 let popup = document.querySelector(".popup");
-let popupOpen = document.querySelector(".profile__edit-button");
-let popupClose = popup.querySelector(".popup__close-button");
-let formElement = document.querySelector(".popup__form");
-let profName = document.querySelector(".profile__title");
-let profJob = document.querySelector(".profile__subtitle");
+let popupOpenBtn = document.querySelector(".profile__edit-button");
+let popupCloseBtn = popup.querySelector(".popup__close-button");
+let formElement= document.querySelector(".popup__form");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
 let nameInput = formElement.querySelector(".popup__input-type_name");
 let jobInput = formElement.querySelector(".popup__input-type_about-self");
 
 function popupToggle() {
   if (popup.classList.contains("popup_opened") === false) {
     popup.classList.add("popup_opened");
-    nameInput.value = profName.textContent;
-    jobInput.value = profJob.textContent;
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
   } else {
     popup.classList.remove("popup_opened");
   }
 }
+
+popupOpenBtn.addEventListener("click", popupToggle);
+popupCloseBtn.addEventListener("click", popupToggle);
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupToggle();
+}
+
+formElement.addEventListener("submit", formSubmitHandler);
 
 function clickOverlay(event) {
   if (event.target === event.currentTarget) {
@@ -23,14 +35,4 @@ function clickOverlay(event) {
   }
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault();
-  profName.textContent = nameInput.value;
-  profJob.textContent = jobInput.value;
-  popupToggle();
-}
-
 popup.addEventListener("click", clickOverlay);
-popupOpen.addEventListener("click", popupToggle);
-popupClose.addEventListener("click", popupToggle);
-formElement.addEventListener("submit", formSubmitHandler);
