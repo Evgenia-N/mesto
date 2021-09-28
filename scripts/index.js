1fab82f2241eef51d15acedcccd90545c8e50a6f
Evgenia-N <jazzpenguin@yandex.ru>
2021-09-28 23:11:06 +0300

    feat: добавлен массив картинок

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 let popup = document.querySelector(".popup");
 let popupOpenBtn = document.querySelector(".profile__edit-button");
 let popupCloseBtn = popup.querySelector(".popup__close-button");
-let formElement= document.querySelector(".popup__form");
+let formElement = document.querySelector(".popup__form");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 let nameInput = formElement.querySelector(".popup__input_type_name");
@@ -17,9 +17,6 @@ function popupToggle() {
   }
 }
 
-popupOpenBtn.addEventListener("click", popupToggle);
-popupCloseBtn.addEventListener("click", popupToggle);
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
@@ -27,31 +24,33 @@ function formSubmitHandler(evt) {
   popupToggle();
 }
 
+popupOpenBtn.addEventListener("click", popupToggle);
+popupCloseBtn.addEventListener("click", popupToggle);
 formElement.addEventListener("submit", formSubmitHandler);
 
 const initialCards = [
   {
-    name: 'Москва',
-    link: 'https://a.radikal.ru/a22/2109/b7/62d26e30b190.jpg'
+    name: "Москва",
+    link: "https://a.radikal.ru/a22/2109/b7/62d26e30b190.jpg",
   },
   {
-    name: 'Обнинск',
-    link: 'https://d.radikal.ru/d15/2109/a8/b7f2f71df658.jpg'
+    name: "Обнинск",
+    link: "https://d.radikal.ru/d15/2109/a8/b7f2f71df658.jpg",
   },
   {
-    name: 'Калуга',
-    link: 'https://a.radikal.ru/a09/2109/dc/b1d907d4b78e.jpg'
+    name: "Калуга",
+    link: "https://a.radikal.ru/a09/2109/dc/b1d907d4b78e.jpg",
   },
   {
-    name: 'Калуга',
-    link: 'https://d.radikal.ru/d04/2109/51/e2355534c3b9.jpg'
+    name: "Калуга",
+    link: "https://d.radikal.ru/d04/2109/51/e2355534c3b9.jpg",
   },
   {
-    name: 'Кижи',
-    link: 'https://a.radikal.ru/a30/2109/2c/98ad26f52f62.jpg'
+    name: "Кижи",
+    link: "https://a.radikal.ru/a30/2109/2c/98ad26f52f62.jpg",
   },
   {
-    name: 'Камчатка',
-    link: 'https://d.radikal.ru/d38/2109/42/790235eb6f84.jpg'
-  }
-]; 
+    name: "Камчатка",
+    link: "https://d.radikal.ru/d38/2109/42/790235eb6f84.jpg",
+  },
+];
