bde073cd179c2e0dda74b76b62342148478767f0
Evgenia <87786550+Evgenia-N@users.noreply.github.com>
2021-10-02 21:11:52 +0300

    fix: исправления в index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,3 @@
-const initialCards = [
-  {
-    name: "Москва",
-    link: "https://a.radikal.ru/a22/2109/b7/62d26e30b190.jpg",
-    alt: "Москва, памятник Минину и Пожарскому на Красной площади.",
-  },
-  {
-    name: "Обнинск",
-    link: "https://d.radikal.ru/d15/2109/a8/b7f2f71df658.jpg",
-    alt: "Обнинск, памятник первопроходцам атомной энергетики.",
-  },
-  {
-    name: "Калуга",
-    link: "https://a.radikal.ru/a09/2109/dc/b1d907d4b78e.jpg",
-    alt: "Калуга, церковь в деревне Никола-Ленивец.",
-  },
-  {
-    name: "Калуга",
-    link: "https://d.radikal.ru/d04/2109/51/e2355534c3b9.jpg",
-    alt: "Калуга, арт-объект в деревне Никола-Ленивец.",
-  },
-  {
-    name: "Кижи",
-    link: "https://a.radikal.ru/a30/2109/2c/98ad26f52f62.jpg",
-    alt: "Карелия, Кижский погост.",
-  },
-  {
-    name: "Камчатка",
-    link: "https://d.radikal.ru/d38/2109/42/790235eb6f84.jpg",
-    alt: "Камчатка, памятник 'Медведи', более известный как 'Здесь начинается Россия'.",
-  },
-];
-
 const popupEditProfile = document.querySelector(".popup_type_edit-profile");
 const popupOpenBtn = document.querySelector(".profile__edit-button");
 const popupCloseBtn = popupEditProfile.querySelector(".popup__close-button");
@@ -43,6 +10,7 @@ const jobInput = formElement.querySelector(".popup__input_type_about-self");
 
 const elements = document.querySelector(".elements");
 const cardTemplateItem = document.querySelector(".card-template");
+
 const popupAddImage = document.querySelector(".popup_type_add-image");
 const containerElement = document.querySelector(".popup__container_type_add-image");
 const popupAddImageOpen = document.querySelector(".profile__add-button");
@@ -51,59 +19,99 @@ const popupAddImageClose = popupAddImage.querySelector(".popup__close-button");
 const popupFullscreenImage = document.querySelector(".popup_type_fullscreen");
 const popupCloseImageBtn = popupFullscreenImage.querySelector(".popup__close-button");
 
-function popupToggle() {
-  if (!popupEditProfile.classList.contains("popup_opened")) {
-    popupEditProfile.classList.add("popup_opened");
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-  } else {
-    popupEditProfile.classList.remove("popup_opened");
-  }
+const initialCards = [
+  {
+    name: "Москва",
+    link: " https://i6.imageban.ru/out/2021/10/02/af5fdc41a3586b4c54fbede45ad36eae.jpg",
+    alt: "Москва, памятник Минину и Пожарскому на Красной площади.",
+  },
+  {
+    name: "Обнинск",
+    link: " https://i1.imageban.ru/out/2021/10/02/2fd9e1b476b773867df375eca69b3e68.jpg",
+    alt: "Обнинск, памятник первопроходцам атомной энергетики.",
+  },
+  {
+    name: "Калуга",
+    link: " https://i7.imageban.ru/out/2021/10/02/e791f4f60f108aa4a0bd59fcfd3dfbb7.jpg",
+    alt: "Калуга, церковь в деревне Никола-Ленивец.",
+  },
+  {
+    name: "Калуга",
+    link: " https://i3.imageban.ru/out/2021/10/02/ef11dbecec5a9add3a4df9f4c9aeb16f.jpg",
+    alt: "Калуга, арт-объект в деревне Никола-Ленивец.",
+  },
+  {
+    name: "Кижи",
+    link: " https://i1.imageban.ru/out/2021/10/02/9ffe54d89dfff790d96d23eb0720726f.jpg",
+    alt: "Карелия, Кижский погост.",
+  },
+  {
+    name: "Камчатка",
+    link: " https://i6.imageban.ru/out/2021/10/02/b8eef552b63c80d17fa26dba86f91cd9.jpg",
+    alt: "Камчатка, памятник 'Медведи', более известный как 'Здесь начинается Россия'.",
+  },
+];
+
+function openPopup(popup) {
+  popup.classList.add("popup_opened");
 }
 
-function formSubmitHandler(evt) {
+function closePopup(popup) {
+  popup.classList.remove("popup_opened");
+}
+
+function openProfile() {
+  openPopup(popupEditProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+popupOpenBtn.addEventListener("click", openProfile);
+popupCloseBtn.addEventListener("click", () => closePopup(popupEditProfile));
+popupAddImageOpen.addEventListener("click", () => openPopup(popupAddImage));
+popupAddImageClose.addEventListener("click", () => closePopup(popupAddImage));
+
+function submitForm(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupToggle();
+  closePopup(popupEditProfile);
 }
+
+formElement.addEventListener("submit", submitForm);
 
 function createCard(item) {
   const card = cardTemplateItem.content.cloneNode(true);
   card.querySelector(".elements__photo").src = item.link;
-  card.querySelector(".elements__photo").alt = item.alt;
+  card.querySelector(".elements__photo").alt = item.alt  || item.name;
   card.querySelector(".elements__title").textContent = item.name;
+
   card.querySelector(".elements__like-button").addEventListener("click", pressLike);
   card.querySelector(".elements__delete-button").addEventListener("click", deleteCard);
   card.querySelector(".elements__photo").addEventListener("click", popupFullscreen);
-
-  elements.prepend(card);
-}
-
-initialCards.reverse();
-initialCards.map(createCard);
-
-function popupImageToggle() {
-  if (!popupAddImage.classList.contains("popup_opened")) {
-    popupAddImage.classList.add("popup_opened");
-  } else {
-    popupAddImage.classList.remove("popup_opened");
-  }
+  return card;
 }
 
 function addCard(evt) {
   evt.preventDefault();
 
-  const addCard = [];
-  addCard.name = evt.currentTarget.querySelector(".popup__input_type_place").value;
-  addCard.link = evt.currentTarget.querySelector(".popup__input_type_url").value;
+  const cardName = evt.currentTarget.querySelector(".popup__input_type_place").value;
+  const cardLink = evt.currentTarget.querySelector(".popup__input_type_url").value;
+  const newCard = createCard({ name: cardName, link: cardLink });
 
-  createCard(addCard);
-  popupImageToggle();
-
-  evt.currentTarget.querySelector(".popup__input_type_place").value = "";
-  evt.currentTarget.querySelector(".popup__input_type_url").value = "";
+  elements.prepend(newCard);
+  closePopup(popupAddImage);
+  event.target.reset();
 }
+
+containerElement.addEventListener("submit", addCard);
+
+function addInitialCard(item) {
+  const initialCard = createCard(item);
+  elements.append(initialCard);
+}
+
+initialCards.map(addInitialCard);
 
 function deleteCard(evt) {
   const elementsCard = evt.currentTarget.closest(".elements__card");
@@ -115,20 +123,13 @@ function pressLike(evt) {
 }
 
 function popupFullscreen(evt) {
-  popupFullscreenImage.classList.add("popup_opened");
-  popupFullscreenImage.querySelector(".popup__image").src = evt.currentTarget.src;
-  popupFullscreenImage.querySelector(".popup__image").alt = evt.currentTarget.alt;
-  popupFullscreenImage.querySelector(".popup__caption").textContent = evt.currentTarget.parentElement.querySelector(".elements__title").textContent;
+  openPopup(popupFullscreenImage);
+  const popupImage = document.querySelector(".popup__image");
+  const popupCaption = document.querySelector(".popup__caption");
+
+  popupImage.src = evt.currentTarget.src;
+  popupImage.alt = evt.currentTarget.alt;
+  popupCaption.textContent = evt.currentTarget.parentElement.querySelector(".elements__title").textContent;
 }
 
-function closeImage() {
-  popupFullscreenImage.classList.remove("popup_opened");
-}
-
-popupOpenBtn.addEventListener("click", popupToggle);
-popupCloseBtn.addEventListener("click", popupToggle);
-formElement.addEventListener("submit", formSubmitHandler);
-popupAddImageOpen.addEventListener("click", popupImageToggle);
-popupAddImageClose.addEventListener("click", popupImageToggle);
-containerElement.addEventListener("submit", addCard);
-popupCloseImageBtn.addEventListener("click", closeImage);
+popupCloseImageBtn.addEventListener("click", () => closePopup(popupFullscreenImage));
