2e47afe1ed7c1412d927c34355d7fbd3a579764d
Evgenia <87786550+Evgenia-N@users.noreply.github.com>
2021-10-03 00:22:13 +0300

    feat: создан файл cards.js

---
+++ scripts/cards.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: "Москва",
+    link: " https://i6.imageban.ru/out/2021/10/02/af5fdc41a3586b4c54fbede45ad36eae.jpg",
+    alt: "Москва, памятник Минину и Пожарскому на Красной площади.",
+  },
+  {
+    name: "Обнинск",
+    link: " https://i1.imageban.ru/out/2021/10/02/2fd9e1b476b773867df375eca69b3e68.jpg",
+    alt: "Обнинск, памятник первопроходцам атомной энергетики.",
+  },
+  {
+    name: "Калуга",
+    link: " https://i7.imageban.ru/out/2021/10/02/e791f4f60f108aa4a0bd59fcfd3dfbb7.jpg",
+    alt: "Калуга, церковь в деревне Никола-Ленивец.",
+  },
+  {
+    name: "Калуга",
+    link: " https://i3.imageban.ru/out/2021/10/02/ef11dbecec5a9add3a4df9f4c9aeb16f.jpg",
+    alt: "Калуга, арт-объект в деревне Никола-Ленивец.",
+  },
+  {
+    name: "Кижи",
+    link: " https://i1.imageban.ru/out/2021/10/02/9ffe54d89dfff790d96d23eb0720726f.jpg",
+    alt: "Карелия, Кижский погост.",
+  },
+  {
+    name: "Камчатка",
+    link: " https://i6.imageban.ru/out/2021/10/02/b8eef552b63c80d17fa26dba86f91cd9.jpg",
+    alt: "Камчатка, памятник 'Медведи', более известный как 'Здесь начинается Россия'.",
+  },
+];
