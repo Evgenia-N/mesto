897a2cccc551cd8f6db61e2d19e16c0849568a2d
Evgenia-N <jazzpenguin@yandex.ru>
2021-11-26 03:02:01 +0300

    fix: confirmDeletionPopup

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,36 +19,3 @@ export const popupFullscreenImage = document.querySelector(".popup_type_fullscre
 export const popupImage = document.querySelector(".popup__image");
 export const popupCaption = document.querySelector(".popup__caption");
 export const popupDeleteCard = document.querySelector(".popup_type_delete-card");
-
-export const initialCards = [
-  {
-    place: "Москва",
-    link: " https://i6.imageban.ru/out/2021/10/02/af5fdc41a3586b4c54fbede45ad36eae.jpg",
-    alt: "Москва, памятник Минину и Пожарскому на Красной площади.",
-  },
-  {
-    place: "Обнинск",
-    link: " https://i1.imageban.ru/out/2021/10/02/2fd9e1b476b773867df375eca69b3e68.jpg",
-    alt: "Обнинск, памятник первопроходцам атомной энергетики.",
-  },
-  {
-    place: "Калуга",
-    link: " https://i7.imageban.ru/out/2021/10/02/e791f4f60f108aa4a0bd59fcfd3dfbb7.jpg",
-    alt: "Калуга, церковь в деревне Никола-Ленивец.",
-  },
-  {
-    place: "Калуга",
-    link: " https://i3.imageban.ru/out/2021/10/02/ef11dbecec5a9add3a4df9f4c9aeb16f.jpg",
-    alt: "Калуга, арт-объект в деревне Никола-Ленивец.",
-  },
-  {
-    place: "Кижи",
-    link: " https://i1.imageban.ru/out/2021/10/02/9ffe54d89dfff790d96d23eb0720726f.jpg",
-    alt: "Карелия, Кижский погост.",
-  },
-  {
-    place: "Камчатка",
-    link: " https://i6.imageban.ru/out/2021/10/02/b8eef552b63c80d17fa26dba86f91cd9.jpg",
-    alt: "Камчатка, памятник 'Медведи', более известный как 'Здесь начинается Россия'.",
-  },
-];
